91ef3069b72cf52e420bde4cf26030d8ef463bcd
Marcos Sousa <sousa.marcoscv@gmail.com>
2021-05-19 19:51:09 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class Program {
 		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.00, dp);
 		System.out.println(seller);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
